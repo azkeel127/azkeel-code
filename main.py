f9b95cbcb78eb1fc0e08107b0f5279522377a7a8
azkeel1267 <91682376+azkeel127@users.noreply.github.com>
2023-10-08 17:10:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/templates/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/templates/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route("/templates/posts")
+@app.route("/posts")
 def posts():
     return render_template("posts.html")
 
